650dd549749c20f59b80efb9515965349a23565c
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-23 20:32:05 +0000

    fixed

--- src/diceRoll.ts
+++ src/diceRoll.ts
@@ -1,6 +1,6 @@
 /**
  * The program is the classic
- * microwave.
+ * diceroll guesser
  *
  * By:      Jackson Naufal
  * Version: 1.0
@@ -11,16 +11,19 @@ import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
 
-// These are my constants for my program
+// These are my constants and variables for my program
 let counter = 1
 const max = 6
 const min = 1
 const randomNumber = Math.floor(Math.random() * max) + min
 
+// this loop will loop an infinite amount of times, till the guess is correct
+// or till the user inputs something stupid, ex "Bob".
 while (true) {
-  // let userInput = soup, pizza, or sub
+  // let userInput = any number less than 6 and greater than or equal to one
   const userInput = prompt('Enter your number (max: 6) (min: 1): ')
   const userGuess = parseFloat(userInput)
+
   // this checks if the user inputted a valid item
   if (userGuess < max || userGuess > min) {
     if (userGuess === randomNumber) {
